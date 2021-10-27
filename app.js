fd7c1633a20ca7a29f788cb1061477efb35d2906
Eros nguyen <cuong123go@gmail.com>
2021-10-27 16:13:07 +0700

    HTML/CSS/JS

---
+++ app.js
@@ -0,0 +1,14 @@
+const About= document.querySelector('.About');
+const Shop =document.querySelector('.Shop');
+About.addEventListener('click',()=>{
+    window.scrollTo({
+        top:1221,
+        behavior: 'smooth'
+    })
+})
+Shop.addEventListener('click',()=>{
+    window.scrollTo({
+        top:1790,
+        behavior: 'smooth'
+    })
+})
